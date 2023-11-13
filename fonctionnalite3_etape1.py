97df947f5fc55dcb3cb4ca050df15a277a11c920
Koucha Mohamed <mohamed.koucha@student-cs.fr>
2023-11-13 14:28:16 +0100

    Update

--- fonctionnalite3_etape1.py
+++ fonctionnalite3_etape1.py
@@ -12,13 +12,22 @@ def get_candidate_queries(num_candidate, file_path):
     try:
         f = open("",'r') #nom du fichier mots clés
         content = f.readlines()
-        result_file = open("",a) #nom du fichier à mettre qui sert de résultat
+        result_file = open("",'a') #nom du fichier à mettre qui sert de résultat
         for ligne in content:
             result = API.tweet_collect_whole(ligne) #remplacer API
             result_file.write("\nresult")
-
         f.close()
         result_file.close()
+
+        hashtags = open("",'r') #nom du fichier hashtags
+        content = hashtags.readlines()
+        result_hashtags = open("",'a') #nom du fichier à mettre qui sert de résultat
+        for ligne in content:
+            result_hash = API.tweet_collect_whole(ligne) #remplacer API
+            result_hashtags.write("\nresult")
+        hashtags.close()
+        result_hashtags.close()
+
     except IOError:
         print("Error")
         pass
