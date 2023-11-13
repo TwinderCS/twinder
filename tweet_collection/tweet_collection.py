487ae2bac421d6c4d00d94201913c597ad609400
Martin.Pt <martin.pupat@student-cs.fr>
2023-11-13 14:13:13 +0100

    Changes to be committed: 	modified:   tweet_collection/tweet_collection.py

--- tweet_collection/tweet_collection.py
+++ tweet_collection/tweet_collection.py
@@ -1,10 +1,12 @@
 import tweepy
+import json
 
 class TweetCollection:
-    def __init__(self):
+    def __init__(self, twitter_connection):
         """
         Will later be used for collections
         """
     
     def store_tweets(self, tweets, filename):
-        raise NotImplementedError
+        with open(filename, "w") as out_file:
+            json.dump(tweets, out_file)
