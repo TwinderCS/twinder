69a9fbc19565fb96640bcaacfcb132b5eed10849
Martin.Pt <martin.pupat@student-cs.fr>
2023-11-13 14:02:41 +0100

    Changes to be committed: 	modified:   tweet_collection/tweet_collection.py

--- tweet_collection/tweet_collection.py
+++ tweet_collection/tweet_collection.py
@@ -5,6 +5,6 @@ class TweetCollection:
         """
         Will later be used for collections
         """
-        
-
     
+    def store_tweets(self, tweets, filename):
+        raise NotImplementedError
