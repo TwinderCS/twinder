e3c51f1b272a65abd6754c5f2f8a8704f00a467e
@anas.lecaillon <anas.lecaillon@student-cs.fr>
2023-11-13 13:43:32 +0100

    corrected gitignore

--- tweet_collection/tweet_collection.py
+++ tweet_collection/tweet_collection.py
@@ -1,10 +1,12 @@
 import tweepy
+import twitter_connection as tc
 
 class TweetCollection:
     def __init__(self):
         """
         Will later be used for collections
         """
+
         
 
     
