61a2b498c519c34e8a0468a15fae55168cd03605
= <=>
2023-11-13 11:15:06 +0100

    helloWorld

---
+++ hello_world.py
@@ -0,0 +1,18 @@
+import tweepy
+# We import our access keys:
+from credentials import *   
+
+def twitter_setup():
+    """
+    Utility function to setup the Twitter's API
+    with an access keys provided in a file credentials.py
+    :return: the authentified API
+    """
+    # Authentication and access using keys:
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+
+    # Return API with authentication:
+    api = tweepy.API(auth)
+    return api
+print(twitter_setup())
