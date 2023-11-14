fdf5f3781113c19f848975fcaa6964a79fff9edf
Koucha Mohamed <mohamed.koucha@student-cs.fr>
2023-11-14 10:14:22 +0100

    Ajout de pandas en alternative pour la collecte du dataset

--- tweet_visualization/seaborn.py
+++ tweet_visualization/seaborn.py
@@ -1,8 +1,10 @@
 import seaborn as sns
+import pandas as pd
 sns.set_theme(style="whitegrid")
 
 # Load the example planets dataset
 dataset = sns.load_dataset("") # Mettre entre guillement le nom du fichier du dataset
+#ou remplacer par dataset = pd.read_csv()
 
 cmap = sns.cubehelix_palette(rot=-.2, as_cmap=True)
 g = sns.relplot(
