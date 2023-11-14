5cb057ad1174da9dc444cb2b42bd2250ad446cd6
@anas.lecaillon <anas.lecaillon@student-cs.fr>
2023-11-14 10:31:42 +0100

    updated words and changed parameters in seaborn to apply to df

--- tweet_visualization/seaborn.py
+++ tweet_visualization/seaborn.py
@@ -1,19 +1,18 @@
 import seaborn as sns
 import pandas as pd
-sns.set_theme(style="whitegrid")
 
 # Load the example planets dataset
-dataset = sns.load_dataset("") # Mettre entre guillement le nom du fichier du dataset
+#dataset = sns.load_dataset("") # Mettre entre guillement le nom du fichier du dataset
 #ou remplacer par dataset = pd.read_csv()
+dataset = pd.read_pickle("../dumps/df.pkl")
 
 cmap = sns.cubehelix_palette(rot=-.2, as_cmap=True)
-g = sns.relplot(
+graph = sns.relplot(
     data=dataset,
-    x="distance", y="orbital_period",
-    hue="year", size="mass",
+    x="date", y="positivity",
     palette=cmap, sizes=(10, 200),
 )
-g.set(xscale="log", yscale="log")
-g.ax.xaxis.grid(True, "minor", linewidth=.25)
-g.ax.yaxis.grid(True, "minor", linewidth=.25)
-g.despine(left=True, bottom=True)
+graph.set(xscale="log", yscale="log")
+graph.ax.xaxis.grid(True, "minor", linewidth=.25)
+graph.ax.yaxis.grid(True, "minor", linewidth=.25)
+graph.despine(left=True, bottom=True)
