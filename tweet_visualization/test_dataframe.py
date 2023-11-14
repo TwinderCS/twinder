49c77f6b7f9d22c52e751f28180d4e3049053ef7
@anas.lecaillon <anas.lecaillon@student-cs.fr>
2023-11-14 10:09:29 +0100

    corrected getLemmaFrom... to get_lemma_from...

--- tweet_visualization/test_dataframe.py
+++
@@ -1,9 +0,0 @@
-from pytest import *
-from tweet_visualization import *
-
-def test_collect():
-    global df
-    #tweets = tweet_collect.collect()
-    #df =  transform_to_dataframe(tweets)
-    assert 'text' in df.columns
-test_collect()
