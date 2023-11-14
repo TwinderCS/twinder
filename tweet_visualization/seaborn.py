5105a326d703c123cda00fc4bdca47e54a89f0b3
Koucha Mohamed <mohamed.koucha@student-cs.fr>
2023-11-14 10:46:05 +0100

    rip seaborn derniere tentavtive

--- tweet_visualization/seaborn.py
+++ tweet_visualization/seaborn.py
@@ -6,11 +6,11 @@ import pandas as pd
 #ou remplacer par dataset = pd.read_csv()
 dataset = pd.read_pickle("../dumps/df.pkl")
 
-cmap = sns.cubehelix_palette(rot=-.2, as_cmap=True)
+#cmap = sns.cubehelix_palette(8)
 graph = sns.relplot(
     data=dataset,
     x="date", y="positivity",
-    palette=cmap, sizes=(10, 200),
+    palette=None, sizes=(10, 200),
 )
 graph.set(xscale="log", yscale="log")
 graph.ax.xaxis.grid(True, "minor", linewidth=.25)
