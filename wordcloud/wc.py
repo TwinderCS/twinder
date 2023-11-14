45809a93a6e24be1a41ca802040693d3b2835163
@anas.lecaillon <anas.lecaillon@student-cs.fr>
2023-11-14 11:23:10 +0100

    created hashtag wordcloud

---
+++ wordcloud/wc.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from wordcloud import WordCloud, STOPWORDS
+from PIL import Image
+
+# Generates a word cloud (taking into account the frequency of words in tweets)
+
+def word_freq(texts):
+    freq = {}
+    for text in texts:
+        for word in text.split(" "):
+            try:
+                if word[0] == "#":
+                    val = freq.get(word.lower(), 0)
+                    freq[word.lower()] = val + 1
+            except:
+                # only if the word is empty
+                pass
+    return freq
+
+def create_image(frequencies):
+    twitter_mask = np.array(Image.open("../dumps/twitter_logo.png"))
+
+    wc = WordCloud(font_path="../dumps/font.ttf", max_words=1000, background_color="white", mask=twitter_mask, stopwords=STOPWORDS)
+    wc.generate_from_frequencies(frequencies)
+
+    plt.imshow(wc, interpolation="bilinear")
+    plt.axis("off")
+    plt.show()
+  
+    
+df = pd.read_pickle("../dumps/df.pkl")
+freq = word_freq(df['text'])
+create_image(freq)
+    
