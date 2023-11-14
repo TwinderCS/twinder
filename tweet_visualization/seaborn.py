94d95daad4445d894a13ad767b8421cea5ca905b
Koucha Mohamed <mohamed.koucha@student-cs.fr>
2023-11-14 10:09:50 +0100

    Ajout de seaborn v1

---
+++ tweet_visualization/seaborn.py
@@ -0,0 +1,17 @@
+import seaborn as sns
+sns.set_theme(style="whitegrid")
+
+# Load the example planets dataset
+dataset = sns.load_dataset("") # Mettre entre guillement le nom du fichier du dataset
+
+cmap = sns.cubehelix_palette(rot=-.2, as_cmap=True)
+g = sns.relplot(
+    data=dataset,
+    x="distance", y="orbital_period",
+    hue="year", size="mass",
+    palette=cmap, sizes=(10, 200),
+)
+g.set(xscale="log", yscale="log")
+g.ax.xaxis.grid(True, "minor", linewidth=.25)
+g.ax.yaxis.grid(True, "minor", linewidth=.25)
+g.despine(left=True, bottom=True)
