c6361abf1f7c94d857f4616f4d31dd3a622c0b50
= <=>
2023-11-14 08:26:34 +0100

    test df

--- tweet_visualization/tweet_visualization.py
+++ tweet_visualization/tweet_visualization.py
@@ -2,8 +2,16 @@ import pandas as pd
 from pandas import DataFrame
 from matplotlib import pyplot as plt
 
-df = pd.read_pickle("../dumps/df.pkl")
+df = pd.read_pickle("dumps/df.pkl")
 
-print(df)
+print(df['text'].iloc[70])
+
+#tfav = pd.Series(data=df['polarity'].values, index=df['date'])
+
+
+# Likes vs retweets visualization:
+#tfav.plot(figsize=(16,4), label="Polarity", legend=True)
+
+#plt.show()
 
 
