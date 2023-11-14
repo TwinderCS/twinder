905a35babd24b69c84c0e5b6a9ba181737490a5e
= <=>
2023-11-14 13:48:53 +0100

    Classifier training with df

--- tweet_visualization/classifierData.py
+++ tweet_visualization/classifierData.py
@@ -1,3 +1,15 @@
+import pandas as pd
+import numpy as np
+import random
+
+df = pd.read_pickle("dumps/df.pkl")
+
+#print(df['user'].to_numpy()[0])
+tweets = df['text'].to_numpy()
+opinions = df['polarity'].to_numpy()
+train_from_df = [(tweets[i], opinions[i]) for i in range(len(tweets))]
+np.random.shuffle(train_from_df)
+train_from_df = train_from_df[0:10000]
 train = [('I love this sandwich.', 'pos'),
     ('this is an amazing place!', 'pos'),
     ('I feel very good about these beers.', 'pos'),
@@ -7,9 +19,8 @@ train = [('I love this sandwich.', 'pos'),
     ('I am tired of this stuff.', 'neg'),
     ("I can't deal with this", 'neg'),
     ('he is my sworn enemy!', 'neg'),
-    ('my boss is horrible.', 'neg')]
-
-test = [('the beer was good.', 'pos'),
+    ('my boss is horrible.', 'neg'),
+    ('the beer was good.', 'pos'),
     ('I do not enjoy my job', 'neg'),
     ("I ain't feeling dandy today.", 'neg'),
     ("I feel amazing!", 'pos'),
--- tweet_visualization/tweet_visualization.py
+++ tweet_visualization/tweet_visualization.py
@@ -7,7 +7,7 @@ from textblob.classifiers import NaiveBayesClassifier
 
 
 #train du classifier
-cl = NaiveBayesClassifier(train)
+cl = NaiveBayesClassifier(train_from_df)
 df = pd.read_pickle("dumps/df.pkl")
 
 #wiki = TextBlob("I hated that guys octopi")
@@ -22,6 +22,7 @@ def get_lemmas_from_tweets(tweets):
                 ret.append(word.lemmatize())
     ret = list(set(ret))
     return ret
+
 def get_opinion_rate(tweets):
     pos_rate, neu_rate, neg_rate = 0, 0, 0
     for tweet in tweets:
@@ -35,9 +36,9 @@ def get_opinion_rate(tweets):
     return (pos_rate/len(tweets), neu_rate/len(tweets), neg_rate/len(tweets))
 
 
-print(get_opinion_rate(["This is cool"]))
+print(get_opinion_rate(["This is cool !"]))
 #print(df.iloc[[0, 99], [0, 3]])
-print(cl.classify("This is cool"))
+#print(cl.classify("This is cool"))
 #print(getLemmasFromTweets(['octopi are cool creatures', 'guys and girls']))
 #print(TextBlob("This is cool").classify())
 """
