e79d7f800879ba84d6134559d1bda278f153d5b8
Martin.Pt <martin.pupat@student-cs.fr>
2023-11-15 10:41:35 +0100

    App changes.

--- dash/app.py
+++ dash/app.py
@@ -3,42 +3,55 @@ from dash import Dash, html, dash_table, dcc, callback, Output, Input
 import pandas as pd
 import plotly.express as px
 import dash_mantine_components as dmc
+from dash_iconify import DashIconify
+import dash_ag_grid as dag
 
 # Incorporate data
 df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
+
+# Icons
+rightswipe_icon = DashIconify(icon="streamline:tinder", width=100)
+leftswipe_icon = DashIconify(icon="streamline:disable-heart", width=100)
 
 # Initialize the app - incorporate a Dash Mantine theme
 external_stylesheets = [dmc.theme.DEFAULT_COLORS]
 app = Dash(__name__, external_stylesheets=external_stylesheets)
 
-# App layout
-app.layout = dmc.Container([
-    dmc.Title('My First App with Data, Graph, and Controls', color="blue", size="h3"),
-    dmc.RadioGroup(
-            [dmc.Radio(i, value=i) for i in  ['pop', 'lifeExp', 'gdpPercap']],
-            id='my-dmc-radio-item',
-            value='lifeExp',
-            size="sm"
-        ),
-    dmc.Grid([
-        dmc.Col([
-            dash_table.DataTable(data=df.to_dict('records'), page_size=12, style_table={'overflowX': 'auto'})
-        ], span=6),
-        dmc.Col([
-            dcc.Graph(figure={}, id='graph-placeholder')
-        ], span=6),
-    ]),
-
-], fluid=True)
-
-# Add controls to build the interaction
-@callback(
-    Output(component_id='graph-placeholder', component_property='figure'),
-    Input(component_id='my-dmc-radio-item', component_property='value')
+dag.AgGrid(
+    className="ag-theme-alpine-dark",
+    # other props
 )
-def update_graph(col_chosen):
-    fig = px.histogram(df, x='continent', y=col_chosen, histfunc='avg')
-    return fig
+
+# App layout
+app.layout = dmc.Stack(
+        spacing="xs",
+        children=[
+            dmc.Title('Le truc Twitter.'),
+            dmc.Card(    
+                withBorder=True,
+                shadow="sm",
+                radius="md",
+                style={"width": 350},
+                children=[
+                    dmc.Stack(
+                        children=[
+                            dmc.Text("Voici un compte bg"),
+                            dmc.SimpleGrid(
+                                cols=2,
+                                children=[
+                                    DashIconify(icon="streamline:tinder", width=100),
+                                    DashIconify(icon="streamline:disable-heart", width=100)
+                                ]
+                            )
+                        ]
+                    )
+                ]
+            )
+        ],
+        align="center"
+    )
+ 
+
 
 # Run the App
 if __name__ == '__main__':
