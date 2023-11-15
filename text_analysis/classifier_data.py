f2cfb0330772aa4c44af7bab911fcb80b8b378cc
= <=>
2023-11-15 08:40:37 +0100

    bug correction after renaming

--- text_analysis/classifier_data.py
+++ text_analysis/classifier_data.py
@@ -5,32 +5,12 @@ import random
 DATAFRAME = pd.read_pickle("dumps/df.pkl")
 
 #print(df['user'].to_numpy()[0])
-TWEETS = df['text'].to_numpy()
-OPINIONS = df['polarity'].to_numpy()
-TRAIN_FROM_DF = [(tweets[i], opinions[i]) for i in range(len(tweets))]
+TWEETS = DATAFRAME['text'].to_numpy()
+OPINIONS = DATAFRAME['polarity'].to_numpy()
+TRAIN_FROM_DF = [(TWEETS[i], OPINIONS[i]) for i in range(len(TWEETS))]
 
-np.random.shuffle(train_from_df)
-<<<<<<< HEAD
-TRAIN_FROM_DF = TRAIN_FROM_DF[0:10000]
-
-TRAIN = [('I love this sandwich.', 'positive'),
-    ('this is an amazing place!', 'positive'),
-    ('I feel very good about these beers.', 'positive'),
-    ('this is my best work.', 'positive'),
-    ("what an awesome view", 'positive'),
-    ('I do not like this restaurant', 'negative'),
-    ('I am tired of this stuff.', 'negative'),
-    ("I can't deal with this", 'negative'),
-    ('he is my sworn enemy!', 'negative'),
-    ('my boss is horrible.', 'negative'),
-    ('the beer was good.', 'positive'),
-    ('I do not enjoy my job', 'negative'),
-    ("I ain't feeling dandy today.", 'negative'),
-    ("I feel amazing!", 'positive'),
-    ('Gary is a friend of mine.', 'positive'),
-    ("I can't believe I'm doing this.", 'negative')]
-=======
-train_from_df = train_from_df[0:10000]
+np.random.shuffle(TRAIN_FROM_DF)
+TRAIN_FROM_DF = TRAIN_FROM_DF[0:10]
 
 train = [('I love this sandwich.', 'pos'),
     ('this is an amazing place!', 'pos'),
@@ -48,4 +28,3 @@ train = [('I love this sandwich.', 'pos'),
     ("I feel amazing!", 'pos'),
     ('Gary is a friend of mine.', 'pos'),
     ("I can't believe I'm doing this.", 'neg')]
->>>>>>> e49cb2ef589d67f43d7b36203059c244eb08e8b7
--- text_analysis/text_analysis.py
+++ text_analysis/text_analysis.py
@@ -2,17 +2,14 @@ import pandas as pd
 from pandas import DataFrame
 from textblob import TextBlob, Word
 from nltk.corpus import stopwords
-<<<<<<< HEAD
-from classifier_data import DATAFRAME, TRAIN_FROM_DF
-=======
 from classifier_data import *
->>>>>>> e49cb2ef589d67f43d7b36203059c244eb08e8b7
 from textblob.classifiers import NaiveBayesClassifier
 
 
 #train du classifier
 def get_classifier():
     cl = NaiveBayesClassifier(TRAIN_FROM_DF)
+    return cl
 
 def get_lemmas_from_tweets(tweets):
     ret = []
@@ -27,6 +24,7 @@ def get_lemmas_from_tweets(tweets):
 def get_opinion_rate(tweets):
     pos_rate, neu_rate, neg_rate = 0, 0, 0
     for tweet in tweets:
+        cl = get_classifier()
         classified = cl.classify(tweet)
         if classified == 'positive':
             pos_rate += 1
@@ -35,13 +33,7 @@ def get_opinion_rate(tweets):
         else:
             neg_rate +=1
     return (pos_rate/len(tweets), neu_rate/len(tweets), neg_rate/len(tweets))
-<<<<<<< HEAD
-=======
 
 
 print(get_opinion_rate(["This is cool !"]))
-#print(df.iloc[[0, 99], [0, 3]])
-#print(cl.classify("This is cool"))
-#print(getLemmasFromTweets(['octopi are cool creatures', 'guys and girls']))
-#print(TextBlob("This is cool").classify())
->>>>>>> e49cb2ef589d67f43d7b36203059c244eb08e8b7
+
