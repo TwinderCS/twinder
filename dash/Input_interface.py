09e21cd98da781947ade2b4841bd389af2d25d59
Koucha Mohamed <mohamed.koucha@student-cs.fr>
2023-11-15 15:27:12 +0100

    On continue le développement pour un bouton et apparence

--- dash/Input_interface.py
+++ dash/Input_interface.py
@@ -1,25 +1,53 @@
 from dash import Dash, dcc, html, Input, Output, callback
+import numpy as np
+import plotly.express as px
+import dash_bootstrap_components as dbc
 
-app = Dash(__name__)
+app = Dash(__name__, external_stylesheets=[dbc.themes.SPACELAB])
 
-app.layout = html.Div([
-    html.H6("Change the value in the text box to see callbacks in action!"),
+heading = html.H4(
+    "Twinder, l'application qui réunit passion et amour", className="bg-primary text-white p-2"
+)
+
+user_id = html.Div(
+    [dbc.Label("User ID",html_for="User"), dbc.Input(id ="user", type="text",value="")],
+    className="mt-2",
+)
+
+control_panel = dbc.Card(
+    dbc.CardBody(
+        [user_id],
+        className="bg-light",
+    )
+)
+
+app.layout = html.Div(
+    [heading,dbc.Row([dbc.Col(control_panel, md = 4)])]
+)
+
+"""app.layout = html.Div(
+    html.Br(),
+    html.Div(id='my-output'))"""
+#user interface object
+"""app.layout = html.Div([
+    html.H6("Twinder"),
     html.Div([
-        "Input: ",
-        dcc.Input(id='my-input', value='initial value', type='text')
+        "User ID: ",
+        dcc.Input(id='my-input', value='', type='text')
     ]),
     html.Br(),
     html.Div(id='my-output'),
 
-])
+])"""
 
 
-@callback(
+
+"""@callback(
     Output(component_id='my-output', component_property='children'),
-    Input(component_id='my-input', component_property='value')
+    Input(component_id='user', component_property='value')
 )
 def update_output_div(input_value):
-    return f'Output: {input_value}'
+    return f'Output: {input_value}'"""
 
 
 if __name__ == '__main__':
