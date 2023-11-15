a159c4bef10e842c151c92c32d1bc0c2ee46553d
Martin.Pt <martin.pupat@student-cs.fr>
2023-11-15 16:15:38 +0100

    Changes to be committed: 	modified:   dash/update_candidate.py 	new file:   images/twinder.svg 	new file:   images/twinder100.png 	new file:   images/twinder1024.png 	new file:   images/twinder8192.png

--- dash/update_candidate.py
+++ dash/update_candidate.py
@@ -1,3 +1,12 @@
+## PLACEHOLDER INITIALIZATION FOR PROFILES
+
+
+
+
+
+
+
+
 # La fonction qui met à jour les candidates quand on appuie sur le bouton de gauche ou de droite.
 # "lr" vaut +1 si on a swipe droite, -1 si on a swipe gauche
 def main(lr, n_clicks):
@@ -5,3 +14,6 @@ def main(lr, n_clicks):
         return "C'est un match ! (" + str(n_clicks) + ")"
     if lr == -1:
         return "Quel dommage... (" + str(n_clicks) + ")"
+    
+
+
