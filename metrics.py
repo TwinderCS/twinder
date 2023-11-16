df3a18e81fc8842107a4607623173183344bf6bb
@anas.lecaillon <anas.lecaillon@student-cs.fr>
2023-11-16 14:29:29 +0100

    create deuclidean distance between user vectors

--- metrics.py
+++ metrics.py
@@ -1,2 +1,8 @@
+import numpy as np
 emotions = ["joy", "sadness", "fear", "anger", "surprise"]
 topics = ["politics", "health", "emotion", "financial", "sport", "science"]
+
+def user_dist(u1, u2):
+    global topics
+    global emotions
+    return np.sqrt(((u1-u2)**2).sum())
