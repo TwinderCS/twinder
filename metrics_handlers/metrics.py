67b4f3968bc57b4f9e621a17cbd2785baa99a6ea
= <balthazar.martin@student-cs.fr>
2023-11-17 10:31:06 +0100

    get_metric_from_tweet implémenté

--- metrics_handlers/metrics.py
+++ metrics_handlers/metrics.py
@@ -11,9 +11,9 @@ max_len = 280
 emotions = ["joy", "sadness", "fear", "anger", "surprise"]
 polarity = ['negative', 'neutral', 'positive']
 topics = ["politics", "health", "emotion", "financial", "sport", "science"]
+alpha = 0.7
 
-
-def emotion_to_vector(emotion):
+def emotion_to_vector(emotion : str):
     match emotion:
         case 'joy':
             return np.array([1, 0, 0, 0, 0])
@@ -27,19 +27,43 @@ def emotion_to_vector(emotion):
             return np.array([0, 0, 0, 0, 1])
     return "Warning: emotion_to_vector : wrong emotion"
 
-def topic_to_vector(topic):
+def topic_to_vector(topic : str):
     for i in range(len(topics)):
         if topic == topics[i]:
             arg = i
     topic_vector = np.zeros(len(topics))
     topic_vector[arg] = 1
-
     return topic_vector
-def get_metrics_from_tweet(tweet):
-    emotion = emotion_model(tweet)
-    topic = topic_model(tweet)
+
+def polarity_to_vector(polarity : str):
+    match polarity:
+        case "positive":
+            return np.array([1, 1, 0, 0, 0])
+        case "neutral":
+            return np.array([1, 1, 0, 0, 0])
+        case "negative":
+            return np.array([0, 0, 0, 1, 1])
+    return "Warning : polarity_to_vector : wrong polarity"
+    return topic_vector
+
+def get_metric_from_tweet(tweet : str, alpha = alpha):
+
     cl = get_classifier()
-    print(cl.classify(tweet))
-#get_metrics_from_tweet("this is cool")
+
+    emotion = 'joy' #emotion_model(tweet)
+    topic = 'politics' #topic_model(tweet)
+    polarity = cl.classify(tweet)
+
+    emotion_vector = emotion_to_vector(emotion)
+    topic_vector = topic_to_vector(topic)
+    pol_vector = polarity_to_vector(polarity)
+
+    mind_state_vector = alpha * pol_vector + (1-alpha) * emotion_vector
+    metrics = np.concatenate((mind_state_vector, topic_vector))
+    print(metrics)
+    #print(polarity_to_vector(pol))
+
+
+get_metric_from_tweet("this is cool")
 #print(emotion_to_vector('fear'))
 #print(topic_to_vector("politics"))
