d6c2df39fd6064b03259a8d5f1b0e6ebefb0e07e
Martin.Pt <martin.pupat@student-cs.fr>
2023-11-23 10:41:02 +0100

    Removed artefacts

--- app/app.py
+++ app/app.py
@@ -72,23 +72,19 @@ def serve_layout():
         with open("app/cookie.txt", "r", encoding="utf-8") as cookie:
             name_d = cookie.readline()
             users_df = user_data_creation(name_d)
-        
         yes_button = dbc.Button("Yes", id='yes-button', color="success", className="mr-2", style={"border-radius": "50%"})
         no_button = dbc.Button("No", id='no-button', color="danger", style={"border-radius": "50%"})
-
         display =  html.Div([
             dcc.Store(id='user-index', data=user_index),
             html.Div(id='user-profile', children=display_user_profile(users_df.iloc[user_index])),
             html.Div([yes_button, no_button], className='d-flex justify-content-center')
             ], className='mt-2', style={'textAlign': 'center'})
-        
         display_panel = dbc.Card(
             dbc.CardBody(
                 [display],
                 className="bg-light",
                 )
-        )
-        
+        )       
         return html.Div(
             [heading,
             dbc.Row([dbc.Col(display_panel, md = 4)],justify = "center"),]
